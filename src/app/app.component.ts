e4e0e12df24df30ef908a6cddd901bf086f99d36
MikaCinc <mihajlo.ls00@outlook.com>
2022-07-08 01:08:42 +0200

    additional drawer items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
 import { AppState } from './app.state';
+import { SpecialCategoryValuesEnum } from './enums';
 import { User } from './models/user';
+import { selectKategorija } from './store/kategorije.action';
+import { loadFeaturedPitanja, loadPitanja } from './store/pitanje.action';
 import { getProfile, logout } from './store/user.actions';
 import { selectUser } from './store/user.selector';
 
@@ -22,7 +26,11 @@ export class AppComponent {
 
   user$: Observable<User | null> = of(null);
 
-  constructor(private store: Store<AppState>, private _snackBar: MatSnackBar) { }
+  constructor(
+    private store: Store<AppState>,
+    private _snackBar: MatSnackBar,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     const token = localStorage.getItem('token');
@@ -43,5 +51,17 @@ export class AppComponent {
     this._snackBar.open("Uspešno ste se odjavili", "Zatvori", {
       duration: 3000,
     });
+  }
+
+  goToFeatured() {
+    this.store.dispatch(selectKategorija({ kategorijaId: SpecialCategoryValuesEnum.FEATURED }));
+    this.store.dispatch(loadFeaturedPitanja());
+    this.router.navigate(['/']);
+  }
+
+  goToAll() {
+    this.store.dispatch(selectKategorija({ kategorijaId: SpecialCategoryValuesEnum.ALL }));
+    this.store.dispatch(loadPitanja());
+    this.router.navigate(['/']);
   }
 }
